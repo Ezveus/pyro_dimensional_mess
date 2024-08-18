7434750b0d1df47412e677a08c69baa272830746
Matthieu Ciappara <ciappa_m@modulotech.fr>
2024-08-18 09:22:32 +0200

    game_over: Prevent error on get_tree()

--- Scenes/Levels/level_1/level_1.gd
+++ Scenes/Levels/level_1/level_1.gd
@@ -4,7 +4,8 @@ func _on_pyromancer_dead():
 	call_deferred("change_scene_to_game_over")
 
 func change_scene_to_game_over():
-	var tree = get_tree()
+	if is_inside_tree():
+		var tree = get_tree()
 	
-	if tree:
-		tree.change_scene_to_file("res://Scenes/Screens/GameOverScreen/game_over_screen.tscn")
+		if tree:
+			tree.change_scene_to_file("res://Scenes/Screens/GameOverScreen/game_over_screen.tscn")
