7c72cf2d11ceb038204a8339e04e09dd3594d70e
Matthieu Ciappara <ciappa_m@modulotech.fr>
2024-08-18 23:30:26 +0200

    level: Add textboxes for tutorial
    - Press Ctrl/Option/Cmd/Alt to cast a fireball

--- Scenes/Levels/level_1/level_1.gd
+++ Scenes/Levels/level_1/level_1.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 @onready var pyromancer: Pyromancer = $Pyromancer
 @onready var start_game_detector: EnvironmentalEventDetector = $StartGameDetector
+@onready var centaur_detector: EnvironmentalEventDetector = $CentaurDetector
+@onready var giant_centaur_detector: EnvironmentalEventDetector = $GiantCentaurDetector
 
 func _ready():
 	start_game_detector.on_entered = show_start_game
+	centaur_detector.on_entered = show_cast_tuto
+	giant_centaur_detector.on_entered = show_size_tuto
 
 func _on_pyromancer_dead():
 	call_deferred("change_scene_to_game_over")
@@ -20,4 +24,10 @@ func _on_castle_door_open():
 	pyromancer.talk("End of the game!")
 
 func show_start_game(body):
-	body.talk("Well! Let's go to this evil sorcerer!")
+	body.talk("Well! Let's jump (press Space) to this evil sorcerer!")
+
+func show_cast_tuto(body):
+	body.talk("Centaurs! If our eyes meet, they'll charge at me. Fortunately, a fireball (press Ctrl) will solve the problem.")
+
+func show_size_tuto(body):
+	body.talk("By Pyrrhos! This centaur is gigantic! Surely an effect of the dimensional spell. I’m going to need a few more fireballs...")
